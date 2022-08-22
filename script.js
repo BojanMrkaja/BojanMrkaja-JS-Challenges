0da0fb9b29e4d5d3ab51d6acf3dbd198324b4b41
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-22 14:29:52 +0200

    Filter out Strings from an Array challenge done

--- script.js
+++ script.js
@@ -259,4 +259,33 @@ const charCount = function (myChar, string) {
   return stringToArr.filter(char => char === myChar).length;
 };
 
-console.log(charCount('b', 'big fat bubble'));
+// console.log(charCount('b', 'big fat bubble'));
+
+/* 
+Filter out Strings from an Array
+
+Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+Examples
+
+filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+
+filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+
+filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+
+Notes
+
+    Zero is a non-negative integer.
+    The given array only has integers and strings.
+    Numbers in the array should not repeat.
+    The original order must be maintained.
+
+*/
+
+const filterArray = function (arr) {
+  const newArr = arr.filter(num => typeof num === 'number');
+
+  return newArr;
+};
+
+console.log(filterArray([1, 'a', 'b', 0, 15]));
