194b16ec0627603e15bdf535c23d7c8eb184a66f
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-23 12:28:10 +0200

    Spotlight Sum challenge don at last, i feel so dumb :)

--- script.js
+++ script.js
@@ -363,4 +363,75 @@ const err = function (num) {
   }
 };
 
-console.log(err(2));
+// console.log(err(2));
+
+/*
+Spotlight Sum
+
+Given a 10x10 grid of numbers 1-100, return the Spotlight Sum, given a number n. The spotlight sum can be defined as the total of the 8 numbers immediately surrounding the number n on the grid, including n in the total.
+Worked Example
+
+[
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+  [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+  [41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+  [51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+  [61, 62, 63, 64, 65, 66, 67, 68, 69, 70],
+  [71, 72, 73, 74, 75, 76, 77, 78, 79, 80],
+  [81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
+  [91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+]
+
+spotlight_sum(45) ➞ 405
+
+// The 8 numbers surrounding 45 on the grid are:
+// [34, 35, 36, 44, 46, 54, 55, 56]
+// Total of the numbers is 360.
+// Include 45 in the total (360 + 45 = 405)
+// Return the answer.
+
+Examples
+
+  spotlightSum(19) ➞ 171
+
+  spotlightSum(48) ➞ 432
+
+  spotlightSum(88) ➞ 792
+
+Notes
+  Note that any numbers which don't have the full 8 numbers surrounding it are not included in the tests.
+*/
+
+const numGrid = [
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+  [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+  [41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+  [51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+  [61, 62, 63, 64, 65, 66, 67, 68, 69, 70],
+  [71, 72, 73, 74, 75, 76, 77, 78, 79, 80],
+  [81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
+  [91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+];
+
+let num1, num2, num3, num4, num5, num6, num7, num8;
+
+const spotlightSum = function (number) {
+  num1 = number - 1;
+  num2 = number + 1;
+  num3 = number - numGrid.length;
+  num4 = num3 - 1;
+  num5 = num3 + 1;
+  num6 = number + numGrid.length;
+  num7 = num6 - 1;
+  num8 = num6 + 1;
+
+  const sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + number;
+
+  return sum;
+};
+
+console.log(spotlightSum(88));
