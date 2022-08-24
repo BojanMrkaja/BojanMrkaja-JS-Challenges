bb2f805700e7407c3e6f09227acaeba98e447693
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-24 11:57:58 +0200

    Sort Numbers in Ascending Order challenge done

--- script.js
+++ script.js
@@ -662,4 +662,41 @@ const sortByLength = function (array) {
   return array.sort((a, b) => a.length - b.length);
 };
 
-console.log(sortByLength(['Turing', 'Einstein', 'Jung']));
+// console.log(sortByLength(['Turing', 'Einstein', 'Jung']));
+
+/*
+Sort Numbers in Ascending Order
+
+Create a function that takes an array of numbers and returns a new array, sorted in ascending order (smallest to biggest).
+
+    Sort numbers array in ascending order.
+    If the function's argument is null, an empty array, or undefined; return an empty array.
+    Return a new array of sorted numbers.
+
+Examples
+
+sortNumsAscending([1, 2, 10, 50, 5]) ➞ [1, 2, 5, 10, 50]
+
+sortNumsAscending([80, 29, 4, -95, -24, 85]) ➞ [-95, -24, 4, 29, 80, 85]
+
+sortNumsAscending(null) ➞ []
+
+sortNumsAscending([]) ➞ []
+
+Notes
+
+Test input can be positive or negative.
+
+*/
+
+const sortNumsAscending = function (arr) {
+  if (arr === null || arr.length === 0) {
+    return [];
+  } else {
+    arr.sort((a, b) => a - b);
+
+    return arr;
+  }
+};
+
+console.log(sortNumsAscending([80, 29, 4, -95, -24, 85]));
