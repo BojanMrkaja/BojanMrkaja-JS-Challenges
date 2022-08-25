5fdee9ef06f836082719d9b8838d1df3814f1b38
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-25 19:44:31 +0200

    Array with No Duplicates challenge done

--- script.js
+++ script.js
@@ -910,14 +910,51 @@ const maximumScore = function (array) {
   return scoreSum;
 };
 
-console.log(
-  maximumScore([
-    { tile: 'N', score: 1 },
-    { tile: 'K', score: 5 },
-    { tile: 'Z', score: 10 },
-    { tile: 'X', score: 8 },
-    { tile: 'D', score: 2 },
-    { tile: 'A', score: 1 },
-    { tile: 'E', score: 1 },
-  ])
-);
+// console.log(
+//   maximumScore([
+//     { tile: 'N', score: 1 },
+//     { tile: 'K', score: 5 },
+//     { tile: 'Z', score: 10 },
+//     { tile: 'X', score: 8 },
+//     { tile: 'D', score: 2 },
+//     { tile: 'A', score: 1 },
+//     { tile: 'E', score: 1 },
+//   ])
+// );
+
+/* 
+Transform into an Array with No Duplicates
+A set is a collection of unique items. A set can be formed from an array by removing all duplicate items.
+
+[1, 3, 3, 5, 5, 5]
+// original array
+
+[1, 3, 5]
+// original array transformed into a set
+
+Create a function that sorts an array and removes all duplicate items from it.
+Examples
+
+set([1, 3, 3, 5, 5]) ➞ [1, 3, 5]
+
+set([4, 4, 4, 4]) ➞ [4]
+
+set([5, 7, 8, 9, 10, 15]) ➞ [5, 7, 8, 9, 10, 15]
+
+set([3, 3, 3, 2, 1]) ➞ [1, 2, 3]
+
+*/
+
+const set = function (arr) {
+  const noDDuplicates = arr.filter((element, index) => {
+    console.log(arr.indexOf(element), element, index);
+    return arr.indexOf(element) === index;
+  });
+
+  return noDDuplicates.sort();
+};
+
+//0 === 0 true, 1 === 1 true, 1 === 2 false , 3 === 3 true, 4 === 4 true , 1 === 5 false, 1 ===6 false, 1===7 false
+// [1, 3, 5, 4];
+
+console.log(set([1, 3, 3, 5, 4, 3, 3, 3]));
