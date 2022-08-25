6a80ddb3f6835d7a02e5e32abaacaf736d133988
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-25 13:35:19 +0200

    Scrabble Hand challenge done

--- script.js
+++ script.js
@@ -844,14 +844,80 @@ const findLargestNums = function (arr) {
   return newArr;
 };
 
+// console.log(
+//   findLargestNums([
+//     [-34, -54, -74],
+//     [-32, -2, -65],
+//     [-54, 7, -43],
+//     [1, 2, , 3, 4],
+//     [1, 2, , 3, 5, 6, 7],
+//     [1, 2, , 3, 5, 6, 8],
+//     [1, 2, , 3, 5, 6, 8],
+//   ])
+// );
+
+/*
+Scrabble Hand
+
+Given an array of scrabble tiles, create a function that outputs the maximum possible score a 
+player can achieve by summing up the total number of points for all the tiles in their hand. Each hand contains 7 scrabble tiles.
+
+Here's an example hand:
+
+[
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 },
+  { tile: "E", score: 1 }
+]
+
+The players maximumScore from playing all these tiles would be 1 + 5 + 10 + 8 + 2 + 1 + 1, or 28.
+Examples
+
+maximumScore([
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 },
+  { tile: "E", score: 1 }
+]) ➞ 28
+
+maximumScore([
+  { tile: "B", score: 2 },
+  { tile: "V", score: 4 },
+  { tile: "F", score: 4 },
+  { tile: "U", score: 1 },
+  { tile: "D", score: 2 },
+  { tile: "O", score: 1 },
+  { tile: "U", score: 1 }
+]) ➞ 15
+
+Notes
+
+Here, each tile is represented as an object with two keys: tile and score.
+*/
+
+const maximumScore = function (array) {
+  let scoreSum = 0;
+
+  array.forEach(element => (scoreSum += element.score));
+
+  return scoreSum;
+};
+
 console.log(
-  findLargestNums([
-    [-34, -54, -74],
-    [-32, -2, -65],
-    [-54, 7, -43],
-    [1, 2, , 3, 4],
-    [1, 2, , 3, 5, 6, 7],
-    [1, 2, , 3, 5, 6, 8],
-    [1, 2, , 3, 5, 6, 8, 10],
+  maximumScore([
+    { tile: 'N', score: 1 },
+    { tile: 'K', score: 5 },
+    { tile: 'Z', score: 10 },
+    { tile: 'X', score: 8 },
+    { tile: 'D', score: 2 },
+    { tile: 'A', score: 1 },
+    { tile: 'E', score: 1 },
   ])
 );
