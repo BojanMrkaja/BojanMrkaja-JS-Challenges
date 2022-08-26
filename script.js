9de3d6987205b5ae7ecf550949c77f564df4f37a
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-26 22:03:59 +0200

    Summing a Slice challenge completed

--- script.js
+++ script.js
@@ -1137,4 +1137,47 @@ const minutesToSeconds = function (string) {
   }
 };
 
-console.log(minutesToSeconds('13:70'));
+// console.log(minutesToSeconds('13:70'));
+
+/*
+Summing a Slice
+
+Given an array and an integer n, return the sum of the first n numbers in the array.
+Worked Example
+
+sliceSum([9, 8, 7, 6], 3) ➞ 24
+// The parameter n is specified as 3.
+// The first 3 numbers in the list are 9, 8 and 7.
+// The sum of these 3 numbers is 24 (9 + 8 + 7).
+// Return the answer.
+
+Examples
+
+sliceSum([1, 3, 2], 2) ➞ 4
+
+sliceSum([4, 2, 5, 7], 4) ➞ 18
+
+sliceSum([3, 6, 2], 0) ➞ 0
+
+Notes
+
+If n is larger than the length of the array, return the sum of the whole array.
+*/
+const sliceSum = function (arr, int) {
+  let sum = 0;
+  const filteredArr = arr.filter((element, index) => index < int);
+
+  for (let i = 0; i < filteredArr.length; i++) {
+    sum += filteredArr[i];
+  }
+
+  if (int > arr.length) {
+    console.log(
+      'Int number is larger then length of array!!!Try anather number'
+    );
+  } else {
+    return sum;
+  }
+};
+
+console.log(sliceSum([1, 3, 2], 2));
