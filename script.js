4eaefa5a2028a2f96d32a394b0b8797ee636efb9
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-27 10:48:05 +0200

    Adding Numbers in a String challenge done

--- script.js
+++ script.js
@@ -1231,4 +1231,41 @@ const chatroomStatus = function (arr) {
   return online;
 };
 
-console.log(chatroomStatus([]));
+// console.log(chatroomStatus([]));
+
+/*
+Adding Numbers in a String
+
+Given a string of numbers separated by a comma and space, return the total sum of all the numbers.
+Examples
+
+addNums("2, 5, 1, 8, 4") ➞ 20
+
+addNums("1, 2, 3, 4, 5, 6, 7") ➞ 28
+
+addNums("10") ➞ 10
+
+Notes
+
+    Numbers will always be separated by a comma and space.
+    Your function should accept negative numbers.
+
+*/
+
+const addNums = function (string) {
+  const splitString = string.split(',');
+  const stringToNumber = [];
+  let sum = 0;
+
+  splitString.forEach(element => {
+    stringToNumber.push(Number(element));
+  });
+
+  stringToNumber.forEach(element => {
+    sum += element;
+  });
+
+  return sum;
+};
+
+console.log(addNums('1, 2, 3, 4, 5, 6, 7'));
