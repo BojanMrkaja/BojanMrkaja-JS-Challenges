ac4da858e31de5d2572ad4930129bd7c0bbad03f
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2022-08-27 11:34:00 +0200

    Spelling it Out challenge done

--- script.js
+++ script.js
@@ -1268,4 +1268,29 @@ const addNums = function (string) {
   return sum;
 };
 
-console.log(addNums('1, 2, 3, 4, 5, 6, 7'));
+// console.log(addNums('1, 2, 3, 4, 5, 6, 7'));
+
+/*
+Spelling it Out
+
+Create a function which takes in a word and spells it out, by consecutively adding letters until the full word is completed.
+Examples
+
+spelling("bee") ➞ ["b", "be", "bee"]
+
+spelling("happy") ➞ ["h", "ha", "hap", "happ", "happy"]
+
+spelling("eagerly") ➞ ["e", "ea", "eag", "eage", "eager", "eagerl", "eagerly"]
+
+*/
+
+const spelling = function (str) {
+  const newArr = [];
+  for (let i = 1; i < str.length + 1; i++) {
+    newArr.push(str.substring(0, i));
+  }
+
+  return newArr;
+};
+
+console.log(spelling('eagerly'));
